f2770a0fe56ad76441a0cf25e4cfd37f68cab840
upallnightcoding <upallnightcoding@gmail.com>
2022-04-10 02:07:14 -0400

    Initial Demo Code

--- Lucy3D/Program.cs
+++ Lucy3D/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace Lucy3D
@@ -10,16 +11,15 @@ namespace Lucy3D
             var nativeWindowSettings = new NativeWindowSettings()
             {
                 Size = new Vector2i(800, 600),
-                Title = "LearnOpenTK - Creating a Window",
+                Title = "LearnOpenTK - Camera",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
             };
 
-            // To create a new window, create a class that extends GameWindow, then call Run() on it.
             using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
             {
                 window.Run();
             }
-
-            // And that's it! That's all it takes to create a window with OpenTK.
         }
     }
 }
--- Lucy3DLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Lucy3DLib
-{
-    public class Class1
-    {
-    }
-}
